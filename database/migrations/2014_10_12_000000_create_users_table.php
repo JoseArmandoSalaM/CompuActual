2f69650c80c74ca65ee8d04436f6873fed58e585
Jose Armando Salazar Moreno <samja1701ap@gmail.com>
2023-08-02 02:40:15 -0600

    Migracion

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->binary('name');
-            $table->binary('email')->unique();
+            $table->binary('email',255)->unique();
             $table->binary('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
